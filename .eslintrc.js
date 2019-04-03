db148290f269b039f2ddfea8d5bd239f40a818fc
Matthew Collier <mcollier@digitalbazaar.com>
2019-04-03 10:14:39 -0400

    Update linting configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "node": true,
+    root: true,
+    env: {
+        node: true,
     },
-    "extends": ["eslint-config-digitalbazaar"]
+    extends: ['digitalbazaar']
 };
